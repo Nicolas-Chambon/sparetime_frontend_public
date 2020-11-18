3b5760769939d2d07e63a028be9fd9b98ae990c5
Florian Poirot <florian.poirot97@gmail.com>
2020-11-18 10:56:48 +0100

    add layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ExtendedModule, FlexModule, GridModule } from "@angular/flex-layout";
+import { GridModule } from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    GridModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, GridModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
